031b57325d9395a1f71a66e56a7ce49a4d8612f7
kudretdonmez <140207033@kocaeli.edu.tr>
2022-10-24 10:40:30 +0300

    aca

---
+++ bolum2/cozum_deneme.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /*double birinci = 15.7;
+  double ikinci = 25.2;
+  double ucuncu = 37.4;
+
+  double ort;
+
+  ort = (birinci + ikinci + ucuncu) / 3;
+  print(ort);
+  */
+}
